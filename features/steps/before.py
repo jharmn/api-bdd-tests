f68e6e3960fc710db858750edb97e924b9d43bbd
Jason Harmon <jason.harmon@gmail.com>
2013-08-26 04:15:14 -0500

    Cleanup

--- features/steps/before.py
+++
@@ -1,9 +0,0 @@
-from lettuce import *
-import ConfigParser
-
-@before.all
-def load_config():
-  Config = ConfigParser.ConfigParser()
-  Config.read("features/config.ini")
-  world.config = Config
-  assert world.config.get('geonames', 'host') is not None
